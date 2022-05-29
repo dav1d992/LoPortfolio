31bf5b498edf89e4451a1e6bb08930e2d431b4b7
dav1d992 <daviden@live.dk>
2022-05-29 06:30:18 +0200

    Add MS Clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import type { DocumentContext } from "next/document";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 import customTheme from "../lib/styles";
 
 export const APP_NAME = "dav1d992";
@@ -21,6 +22,16 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "c3tani8yyi");`,
+          }}
+        />
       </Html>
     );
   }
